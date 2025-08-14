7ed9181500a01f0d167f2de8652f9fca6837c386
phamvanthe94 <mihthe05121994@gmail.com>
2025-08-14 11:39:15 +0900

    Nộp bài tập SS13_ex01

---
+++ SS13_ex01.js
@@ -0,0 +1,6 @@
+let hobbies = [` Nấu ăn`, ` Đọc sách`, ` Thể thao`, ` Du lịch`, ` Học tập`];
+
+hobbies.splice(0, 0, ` Hát`, ` Nhảy`, ` Tập võ`);
+hobbies.unshift(`Nghe nhạc`);
+hobbies.push(`Đàn`);
+console.log(hobbies);
